939154852a626dc62266682ef15df53c931596a9
thisan <thiago.thisan@gmail.com>
2020-07-28 12:20:23 -0300

    finalizando third_card

--- lib/pages/widgets/page_view_app.dart
+++ lib/pages/widgets/page_view_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nubank_clone/pages/widgets/card_app.dart';
 import 'package:nubank_clone/pages/widgets/first_card.dart';
 import 'package:nubank_clone/pages/widgets/second_card.dart';
+import 'package:nubank_clone/pages/widgets/third_card.dart';
 
 class PageViewApp extends StatelessWidget {
   final double top;
@@ -36,7 +37,9 @@ class PageViewApp extends StatelessWidget {
             CardApp(
               child: SecondCard(),
             ),
-            CardApp(),
+            CardApp(
+              child: ThirdCard(),
+            ),
           ],
         ),
       ),
